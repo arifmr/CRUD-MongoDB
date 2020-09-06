d7be070c7a3fea18869315db13c577a9ece483a0
arifmr <arifmr091@gmail.com>
2020-09-06 13:38:46 +0700

    first init

---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
+
+func main() {
+	r := gin.Default()
+
+	p := r.Group("/user")
+	{
+		p.GET("/", Get)
+		p.GET("/:id", GetByID)
+		p.POST("/create", Create)
+		p.PUT("/:id", Update)
+		p.DELETE("/:id", Delete)
+	}
+	r.Run(":8080")
+}
+
+func Get(c *gin.Context) {
+	client, err := db.Mongodb()
+	if err != nil {
+		c.String(500, err.Error())
+		return
+	}
+
+	result, err := client.Database("user").Collection("user").Find(context.Background(), bson.M{})
+	if err != nil {
+		c.String(500, err.Error())
+		return
+	}
+
+	var data []map[string]interface
+	result.All(context.Background(), &data)
+
+	c.JSON(200, data)
+}
+
+func GetByID(c *gin.Context) {
+	id := c.Param("id")
+
+	client, err := db.Mongodb()
+	if err != nil {
+		c.String(500, err.Error())
+		return
+	}
+
+	result, err := client.Database("user").Collection("user").Find(context.Background(), bson.M{"username": id})
+	if err != nil {
+		c.String(500, err.Error())
+		return
+	}
+
+	var data map[string]interface{}
+	result.All(context.Background(), &data)
+
+	c.JSON(200, data)
+}
+
+func Create(c *gin.Context) {
+	var user User
+	c.BindJSON(&user)
+
+	client, err := db.Mongodb()
+	if err != nil {
+		c.String(200, err.Error())
+		return
+	}
+
+	_, err = client.Database("user").Collection("user").InsertOne(context.Background(), user)
+	if err != nil {
+		c.String(500, err.Error())
+		return
+	}
+
+	c.String(200, "success")
+}
+
+func Update(c *gin.Context) {
+	var user User
+	c.BindJSON(&user)
+
+	client, err := db.Mongodb()
+	if err != nil {
+		c.String(500, err.Error())
+		return
+	}
+
+	_, err = client.Database("user").Collection("user").UpdateOne(context.Background(), bson.M{"username": user.Username}, bson.M{"$set" : user})
+	if err != nil {
+		c.String(500, err.Error())
+		return
+	}
+
+	c.String(200, "success")
+}
+
+func Delete(c *gin.Context) {
+	id := c.Param("id")
+
+	client, err := db.Mongodb()
+	if err != nil {
+		c.String(500, err.Error())
+		return
+	}
+
+	_, err = client.Database("user").Collection("user").DeleteOne(context.Background(), bson.M{"username": id})
+	if err != nil {
+		c.String(500, err.Error())
+		return
+	}
+
+	c.String(200, "success")
+}
